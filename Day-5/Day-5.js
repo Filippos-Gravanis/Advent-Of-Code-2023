6062cb438ce3c18735c6f7b6c74eb0112bcd12c9
Filippos Gravanis <grampouni20@gmail.com>
2023-12-19 18:17:38 +0200

    made part 1 and part 2 but part 2 is very slow

--- Day-5/Day-5.js
+++ Day-5/Day-5.js
@@ -12,27 +12,97 @@ let firstLine = input[0][0].split(":")
 input = input.slice(1,input.length)
 input.unshift(firstLine)
 
-function seedToSoil (seeds,soils) {
+function findMatches (items,map) {
     let matches = []
-    let seedMap= []
-    let soilMap=[]
-    soils = soils.splice(1,soils.length)
-    soils = soils.map(line => {
-        return line.split(" ")
+    items.forEach( item  => {
+        let match 
+        map.forEach(mapItem => {
+            mapItem = mapItem.split(" ")
+            if ( parseInt(item) >= parseInt(mapItem[1]) && parseInt(item) < parseInt( mapItem[1])+ parseInt(mapItem[2]) ){
+                match =  parseInt(mapItem[0]) + (parseInt(item) - parseInt(mapItem[1]))
+            }
+        })
+        if (match){
+        matches.push(match)
+        }
+        else{
+            matches.push(parseInt(item))
+        }
+        
     })
-    soils.forEach( soil => {
-        console.log(soil)
-        for (let range = 0 ; range < soil[2] ; range++){
-            seedMap.push(parseInt(soil[1])+range)
-            soilMap.push(parseInt(soil[0])+range)
+    return matches
+}
+
+
+function findRangeMatches (items) {
+    let minLocation = findLocation(items[0][0])
+    items.forEach(item => {
+        for (let index = parseInt(item[0]) ; index < parseInt(item[0])+parseInt(item[1]); index++ ){
+            indexNumberLocation =  findLocation(index)
+            if (indexNumberLocation < minLocation){
+                minLocation = indexNumberLocation
+            }
         }
     })
-    console.log("seedMap",seedMap,"soilMap",soilMap)
-    seeds = seeds.split(" ")
-    seeds.forEach(seed => {
-        console.log(seed)
-
-    })
+    console.log(minLocation,"minLocation")
 }
-seedToSoil(input[0][1],input[1])
 
+
+
+function findLocation (item ) {
+   let soil = findMatches([item],input[1].slice(1,input[1].length))
+   let fertilizer = findMatches(soil,input[2].slice(1,input[2].length))
+   let water = findMatches(fertilizer,input[3].slice(1,input[3].length))
+   let light = findMatches(water,input[4].slice(1,input[4].length))
+   let  temperature = findMatches(light,input[5].slice(1,input[5].length))
+   let humidity = findMatches(temperature,input[6].slice(1,input[6].length))
+   let location  = findMatches(humidity,input[7].slice(1,input[7].length))
+   return location
+}
+
+
+
+let rangeSeeds = []
+let index = 0
+let rangeInput = input[0][1].trim().split(" ")
+let pairIndex = 1
+let pair = []
+while (index<rangeInput.length){
+    
+    if (pairIndex == 2){
+        pair.push(rangeInput[index])
+        rangeSeeds.push(pair)
+        pairIndex = 0
+        pair = []
+    }
+    else{
+        pair.push(rangeInput[index])
+    }
+    pairIndex++
+    index++
+}
+
+findRangeMatches(rangeSeeds)
+
+// let seedList = []
+// for (let listIndex = 0 ; listIndex < rangeSeeds.length; listIndex++){
+//     console.log(parseInt(rangeSeeds[listIndex][0]))
+//     for (let numberIndex = parseInt(rangeSeeds[listIndex][0]) ; numberIndex < parseInt(rangeSeeds[listIndex][0]) + 
+//     parseInt(rangeSeeds[listIndex][1]) ;
+//      numberIndex++)
+//     {
+//         seedList.h(numberIndex)
+//     }
+// }
+
+
+let soils = findMatches(input[0][1].trim().split(" "),input[1].slice(1,input[1].length))
+let fertilizer = findMatches(soils,input[2].slice(1,input[2].length))
+let water = findMatches(fertilizer,input[3].slice(1,input[3].length))
+let light = findMatches(water,input[4].slice(1,input[4].length))
+let temperature = findMatches(light,input[5].slice(1,input[5].length))
+let humidity = findMatches(temperature,input[6].slice(1,input[6].length))
+let location = findMatches(humidity,input[7].slice(1,input[7].length))
+
+
+console.log (Math.min(...location))
